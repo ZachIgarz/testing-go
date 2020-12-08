0f7bb96d439cc930a4752fc513cce2f89abe4c9a
zach <huffyh00@hotmail.com>
2020-12-08 12:04:27 -0500

    feat: benchmark

--- clase1/suma_test.go
+++ clase1/suma_test.go
@@ -6,21 +6,24 @@ import "testing"
 ejecutar una funcion especifica
 go test -v run  (nombre funcion)
 go test -v run  (nombre funcion)$  final de la exp
+go test -v muestra logs
 */
 func TestAdd(t *testing.T) {
-	want := 9
+	want := 5
+	t.Logf("want vale %d \n", want)
 	got := Add(2, 3)
+	t.Logf("got vale %d \n", got)
 	if got != want {
-		t.Logf("error : se esperab %d se obtuvo %d", want, got)
-		t.Fail()
+		t.Errorf("error : se esperab %d se obtuvo %d", want, got)
+
 	}
+	t.Log("Termino la prueba de Add")
 }
 
 func TestAddMultiple(t *testing.T) {
 	want := 6
 	got := AddMultiple(1, 2, 3)
 	if got != want {
-		t.Logf("error : se esperab %d se obtuvo %d", want, got)
-		t.Fail()
+		t.Errorf("error : se esperab %d se obtuvo %d", want, got)
 	}
 }
